63051028c46444f6770e5273c9b9d36262319672
dimo2008 <bio.eng.ahmed.magdy@gmail.com>
2025-11-17 18:32:33 +0200

    add member seed

--- frontend/src/app/member/member.ts
+++ frontend/src/app/member/member.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-member',
@@ -6,6 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './member.html',
   styleUrl: './member.css',
 })
-export class Member {
+export class Member implements OnInit {
+  name: string = '';
+  phone: string = '';
+  email: string = '';
+  address: string = '';
+  hobbies: string[] = [];
+  gender: string = '';
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.name = 'Ahmed Magdy';
+    this.phone = '01012345678';
+    this.email = 'ahmad@gmail.com';
+    this.address = 'Cairo, Egypt';
+    this.hobbies = ['Reading', 'Traveling', 'Coding'];
+    this.gender = 'Male';
+  }
 }

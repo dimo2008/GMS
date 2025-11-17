f9e0af4943479bdaf39fc35604785bcd649d6be2
dimo2008 <bio.eng.ahmed.magdy@gmail.com>
2025-11-17 19:31:35 +0200

    apply routing

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Member } from './member/member';
 import { Team } from './team/team';
+import { Sidebar } from './sidebar/sidebar';
+import { Dashboard } from './dashboard/dashboard';
+import { About } from './about/about';
 
 @NgModule({
   declarations: [
     App,
     Member,
-    Team
+    Team,
+    Sidebar,
+    Dashboard,
+    About
   ],
   imports: [
     BrowserModule,
